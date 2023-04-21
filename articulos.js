4bf156ce0eb524d47d1cc4859379807649df69a1
nicolasesmite <110636397+nicolasesmite@users.noreply.github.com>
2023-04-21 20:01:13 -0300

    actualizacion

---
+++ articulos.js
@@ -0,0 +1,66 @@
+let Articulos= [
+    {
+      "nombre": "Mate Imperial",
+      "precio": 3500,
+      "moneda": "UYU",
+      "stock": 25,
+      "imagen": "",
+      "id" : 1717178,
+    },
+  
+    {
+      "nombre": "Mate Camionero",
+      "precio": 2200,
+      "moneda": "UYU",
+      "stock": 25,
+      "imagen": "",
+      "id" : 161616,
+  
+    },
+    {
+      "nombre": "Mate Guampa",
+      "precio": 1000,
+      "moneda": "UYU",
+      "stock": 25,
+      "imagen": "",
+      "id" : 171717,
+  
+    },
+    {
+      "nombre": "Mate Ceramica",
+      "precio": 780,
+      "moneda": "UYU",
+      "stock": 25,
+      "imagen": "",
+      "id" : 15151515,
+  
+    },
+    {
+      "nombre": "Bombilla de Plata",
+      "precio": 1500,
+      "moneda": "UYU",
+      "stock": 20,
+      "imagen": "",
+      "id" : 141414,
+    },
+  
+    {
+      "nombre": "Bombilla de Bronce",
+      "precio": 1350,
+      "moneda": "UYU",
+      "stock": 20,
+      "imagen": "",
+      "id": 13131313,
+  
+    },
+    {
+      "nombre": "Bombilla de Alpaca",
+      "precio": 1000,
+      "moneda": "UYU",
+      "stock": 20,
+      "imagen": "",
+      "id" : 121212 ,
+  
+    }
+  
+  ]
