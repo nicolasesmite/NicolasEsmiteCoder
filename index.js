c20c7d9b3c22a1e75cc4db1d42dec2c61aa74695
nicolasesmite <110636397+nicolasesmite@users.noreply.github.com>
2023-04-21 21:38:29 -0300

    quitamos articulos.js

--- index.js
+++ index.js
@@ -8,6 +8,73 @@ const btnSearch = document.getElementById("botonBuscar")
 const informacion = document.getElementById("Informacion")
 let carrito = [];
 
+let Articulos= [
+  {
+    "nombre": "Mate Imperial",
+    "precio": 3500,
+    "moneda": "UYU",
+    "stock": 25,
+    "imagen": "",
+    "id" : 1717178,
+  },
+
+  {
+    "nombre": "Mate Camionero",
+    "precio": 2200,
+    "moneda": "UYU",
+    "stock": 25,
+    "imagen": "",
+    "id" : 161616,
+
+  },
+  {
+    "nombre": "Mate Guampa",
+    "precio": 1000,
+    "moneda": "UYU",
+    "stock": 25,
+    "imagen": "",
+    "id" : 171717,
+
+  },
+  {
+    "nombre": "Mate Ceramica",
+    "precio": 780,
+    "moneda": "UYU",
+    "stock": 25,
+    "imagen": "",
+    "id" : 15151515,
+
+  },
+  {
+    "nombre": "Bombilla de Plata",
+    "precio": 1500,
+    "moneda": "UYU",
+    "stock": 20,
+    "imagen": "",
+    "id" : 141414,
+  },
+
+  {
+    "nombre": "Bombilla de Bronce",
+    "precio": 1350,
+    "moneda": "UYU",
+    "stock": 20,
+    "imagen": "",
+    "id": 13131313,
+
+  },
+  {
+    "nombre": "Bombilla de Alpaca",
+    "precio": 1000,
+    "moneda": "UYU",
+    "stock": 20,
+    "imagen": "",
+    "id" : 121212 ,
+
+  }
+
+]
+
 mostrarArticulos(Articulos)
 
 function agregarAlCarrito(idCarro) {
